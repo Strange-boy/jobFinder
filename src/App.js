a79da31b6df05783cc4e21048a5ef676f0140842
Joel <joel.panvel@gmail.com>
2023-11-02 17:53:14 +0530

    Latest way of writing the React router dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {
+	Route,
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+} from "react-router-dom";
 
 //in order to import the pages
 import Home from "./pages/Home";
 import About from "./pages/About";
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<RootLayout />}>
+			<Route index element={<Home />} />
+			<Route path="about" element={<About />} />
+		</Route>
+	)
+);
 
 function App() {
-	return (
-		<BrowserRouter>
-			<header>
-				<nav>
-					<NavLink to="/">Home</NavLink>
-					<NavLink to="/about">About</NavLink>
-				</nav>
-			</header>
-			<main>
-				<Routes>
-					<Route index element={<Home />} />
-					<Route path="about" element={<About />} />
-				</Routes>
-			</main>
-		</BrowserRouter>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
