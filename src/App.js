72f5b49858a91ed14cd3616198154a925cc91e2a
Joel <joel.panvel@gmail.com>
2023-11-02 17:17:54 +0530

    Learning about the initial ways of routing files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+
+//in order to import the pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
-  return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<header>
+				<nav>
+					<NavLink to="/">Home</NavLink>
+					<NavLink to="/about">About</NavLink>
+				</nav>
+			</header>
+			<main>
+				<Routes>
+					<Route index element={<Home />} />
+					<Route path="about" element={<About />} />
+				</Routes>
+			</main>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
