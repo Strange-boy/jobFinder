0727c5a6599b862e6cc4044fdc0a329901ae4b37
Joel <joel.panvel@gmail.com>
2023-11-11 13:40:59 +0530

    using form actions in react router dom

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/help/Faq";
-import Contact from "./pages/help/Contact";
+import Contact, { contactActionForm } from "./pages/help/Contact";
 import NotFound from "./pages/NotFound";
 import CareerDetails, {
 	careerDetailsLoader,
@@ -29,7 +29,11 @@ const router = createBrowserRouter(
 			<Route path="about" element={<About />} />
 			<Route path="help" element={<HelpLayout />}>
 				<Route path="faq" element={<Faq />} />
-				<Route path="contact" element={<Contact />} />
+				<Route
+					path="contact"
+					element={<Contact />}
+					action={contactActionForm}
+				/>
 			</Route>
 
 			<Route
